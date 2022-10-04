6f5bd49ffcb5c60fa400931d5389a9a74c58af8b
juanfrg2001 <juanfrg2001@gmail.com>
2022-10-04 15:34:53 -0500

    Adds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :cellphone_brand
+  root 'cellphone_brand#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
